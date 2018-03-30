6e763586d9bd3f010a49e8b9af0e563087301ccb
Jeremy Potter <git@stormdesign.us>
2018-03-30 15:50:33 -0700

    Minor bugfix

--- ubuntucleaner/janitor/aptcache_plugin.py
+++ ubuntucleaner/janitor/aptcache_plugin.py
@@ -15,13 +15,14 @@ class AptCachePlugin(JanitorCachePlugin):
 
     def clean_cruft(self, cruft_list=[], parent=None):
         for index, cruft in enumerate(cruft_list):
-            log.debug('Cleaning...%s' % cruft.get_name())
-            result = proxy.delete_apt_cache_file(cruft.get_name())
+            if cruft:
+                log.debug('Cleaning...%s' % cruft.get_name())
+                result = proxy.delete_apt_cache_file(cruft.get_name())
 
-            if bool(result) == False:
-                self.emit('clean_error', cruft.get_name())
-                break
-            else:
-                self.emit('object_cleaned', cruft, index + 1)
+                if bool(result) == False:
+                    self.emit('clean_error', cruft.get_name())
+                    break
+                else:
+                    self.emit('object_cleaned', cruft, index + 1)
 
         self.emit('all_cleaned', True)
