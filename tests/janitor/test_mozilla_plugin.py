b7f11e257e31255e61449e27aa338d6b9993fe37
gerardpuig <gerardpuig@users.noreply.github.com>
2020-03-30 19:45:50 +0200

    increased mozilla plugin janitor test to 100%

--- tests/janitor/test_mozilla_plugin.py
+++ tests/janitor/test_mozilla_plugin.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+import mock
 from ubuntucleaner.janitor.mozilla_plugin import FirefoxCachePlugin
 
 
@@ -9,6 +10,14 @@ class TestMozillaCachePlugin(unittest.TestCase):
         self.firefox_plugin = FirefoxCachePlugin()
 
     def test_firefox_plugin(self):
+        self.assertTrue(
+            os.path.expanduser('~/.mozilla/firefox/5tzbwjwa.default'),
+            self.firefox_plugin.get_path()
+        )
+
+    @mock.patch('ubuntucleaner.settings.common.RawConfigSetting.sections',
+                mock.Mock(side_effect=Exception))
+    def test_firefox_plugin_path_exception(self):
         self.assertTrue(
             os.path.expanduser('~/.mozilla/firefox/5tzbwjwa.default'),
             self.firefox_plugin.get_path()
